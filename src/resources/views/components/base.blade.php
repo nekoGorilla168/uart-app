22bc03d9d7796a5f01552ff0aae95c383098abf3
DaisukeTacihara <tachihara.29464@gmail.com>
2021-07-07 00:12:03 +0900

    ホーム画面の基本的な構成を作成

--- src/resources/views/components/base.blade.php
+++ src/resources/views/components/base.blade.php
@@ -8,9 +8,13 @@
         <title>{{ $title }}</title>
     </head>
     <body>
-        {{-- <x-header></x-header> --}}
-        <main>{{ $slot }}</main>
-        <footer>&copy; 2021 daisuke tachihara</footer>
+        {{ $header }}
+        {{ $slot }}
+        <footer class="p-5 text-center bg-light">
+            <div class="container">
+                <p>Copyrigt &copy; 2021 daisuke tachihara</p>
+            </div>
+        </footer>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     </body>
 </html>
